f7f10706035227c57761a96af22ba953a3287f1f
goodmattg <mattgoodman13@gmail.com>
2016-02-18 13:37:34 -0500

    form complete

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/:username', function(req, res, next) {
     res.render('users');
-    console.log(req.params.username);
 });
 
 module.exports = router;
