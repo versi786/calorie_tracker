0ef5d58dd7aaa57aab2ff82605de2c7c21da318d
goodmattg <mattgoodman13@gmail.com>
2016-02-25 21:47:53 -0500

    cleaned newEntry GET

--- routes/newEntry.js
+++ routes/newEntry.js
@@ -10,21 +10,11 @@ router.get('/', function(req, res, next) {
     // If not in user session re-direct to home-page. Still a vulernability - fairly certain the library doesn't hash when inserting data into the session-ID. If I knew the username I could manually construct a request object with the username in the sessionId.
     if (!req.session.user) {
       res.redirect('../');
-    }
-
-    var goalsql = 'SELECT carbs, fat, protein FROM users WHERE username = ?';
-    var inserts = [req.params.username];
-    goalsql = mysql.format(goalsql, inserts);
-
-    db.query(goalsql, function(err, rows, fields) {
-      if(err) {
-          req.session.error = 'database error';
-          res.redirect('/');
-      }
+    }else{
       res.render('newEntry',{username: req.session.user,
                   error: req.session.error});
       req.session.error = null;
-    });
+    }
 });
 
 router.post('/', function(req, res, next) {
