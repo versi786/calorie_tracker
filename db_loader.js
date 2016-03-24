32211caa64da59b0c47570bb1f4531592cb886aa
Aasif Versi <versia@seas.upenn.edu>
2016-03-24 19:43:44 -0400

    fixed merge conflicts

--- db_loader.js
+++ db_loader.js
@@ -90,7 +90,6 @@ function createTables() {
     console.log(fields);
   });
 
-<<<<<<< HEAD
   // db.query('CREATE TABLE favorites ( \
   //           fav_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,\
   //           name CHARACTER(100) NOT NULL,\
@@ -120,19 +119,18 @@ function createTables() {
   //   console.log(rows);
   //   console.log(fields);
   // });
-=======
-  db.query('ALTER TABLE exercise \
-            ADD CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES users(username) \
-            ON UPDATE CASCADE \
-            ON DELETE CASCADE;', function(err, rows, fields) {
-    if(err) {
-      throw err;
-    }
-    console.log("Foreign key constraint added to exercise");
-    console.log(rows);
-    console.log(fields);
-  });
->>>>>>> 43058154833aa2adaeab84dc9050be1ffaa9f638
+
+  // db.query('ALTER TABLE exercise \
+  //           ADD CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES users(username) \
+  //           ON UPDATE CASCADE \
+  //           ON DELETE CASCADE;', function(err, rows, fields) {
+  //   if(err) {
+  //     throw err;
+  //   }
+  //   console.log("Foreign key constraint added to exercise");
+  //   console.log(rows);
+  //   console.log(fields);
+  // });
 
   return;
 }
